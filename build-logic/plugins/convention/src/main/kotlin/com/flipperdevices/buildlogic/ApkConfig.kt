321729fc71eb2f6ceb3b9d0356226488fd45407a
atomofiron <atomofiron@gmail.com>
2023-08-06 15:15:48 +0400

    edit DEBUG_VERSION

--- build-logic/plugins/convention/src/main/kotlin/com/flipperdevices/buildlogic/ApkConfig.kt
+++ build-logic/plugins/convention/src/main/kotlin/com/flipperdevices/buildlogic/ApkConfig.kt
@@ -12,7 +12,7 @@ object ApkConfig {
     const val COMPILE_SDK_VERSION = 36
     const val ROBOELECTRIC_SDK_VERSION = 34
 
-    private const val DEBUG_VERSION = "DEBUG_VERSION"
+    private const val DEBUG_VERSION = "GitHub master 27.04.2026"
 
     val Project.VERSION_CODE
         get() = prop("version_code", Integer.MAX_VALUE).toInt()
